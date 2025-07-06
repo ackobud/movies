1df3bd375a8d07b9eb1869691dbc81fb17708876
ackobud <ackobud@gmail.com>
2025-07-06 21:00:41 +0200

    finalised

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Movies = () => {
-  return <></>;
+  const [movieName, setMovieName] = useState();
+  const searchMovieByTitle = () => {
+    axios
+      .get(
+        `${process.env.REACT_APP_OMDBAPI_URL}?t=${movieName}&apikey=${process.env.REACT_APP_OMDBAPI_KEY}`
+      )
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
+  return (
+    <>
+      <form>
+        <input
+          type="text"
+          onInput={(e) => setMovieName(e.currentTarget.value)}
+        />
+        <button onClick={searchMovieByTitle} type="button">
+          Search movies
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Movies;
