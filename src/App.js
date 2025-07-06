1b7a97b5b6d04b5340af5e4fc1ccda21f37f7f19
ackobud <ackobud@gmail.com>
2025-07-06 20:47:20 +0200

    separating logic into components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
-
-// https://www.omdbapi.com/?t=Terminator&apikey=7c213dd
-console.log(
-  process.env.REACT_APP_OMDBAPI_KEY,
-  process.env.REACT_APP_OMDBAPI_URL
-);
-
-// fetch("https:www.omdbapi.com/?t=Terminator&apikey=7c213dd")
-//   .then((response) => response.json())
-//   .then((jsonResponse) => console.log(jsonResponse));
+import Movies from "./Components/Movies";
 
 axios
   .get(
@@ -21,25 +11,12 @@ axios
   .then((response) => console.log(response))
   .catch((error) => console.log(error));
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Movies />
+    </>
   );
-}
+};
 
 export default App;
