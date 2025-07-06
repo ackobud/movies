42ea2389c63569cdb4f220e559405de7bcd7eb8c
ackobud <ackobud@gmail.com>
2025-07-06 20:41:15 +0200

    making movie app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+// https://www.omdbapi.com/?t=Terminator&apikey=7c213dd
+console.log(
+  process.env.REACT_APP_OMDBAPI_KEY,
+  process.env.REACT_APP_OMDBAPI_URL
+);
+
+// fetch("https:www.omdbapi.com/?t=Terminator&apikey=7c213dd")
+//   .then((response) => response.json())
+//   .then((jsonResponse) => console.log(jsonResponse));
+
+axios
+  .get(
+    process.env.REACT_APP_OMDBAPI_URL +
+      "?t=Terminator&apikey=" +
+      process.env.REACT_APP_OMDBAPI_KEY
+  )
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error));
 
 function App() {
   return (
