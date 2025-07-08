d95c7a0a9a48399dbdcabd6877da651f55b42685
ackobud <ackobud@gmail.com>
2025-07-08 21:45:16 +0200

    added components and basic layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import axios from "axios";
 import Movies from "./Components/Movies";
+import Navigation from "./Templates/Snippets/Navigation";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./Templates/Snippets/Footer";
 
 const App = () => {
   return (
     <>
+      <Navigation />
       <Movies />
+      <Footer />
     </>
   );
 };
---
+++ src/Templates/Snippets/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="d-flex justify-content-center align-items-center bg-dark text-white p-4">
+      <p className="m-8">Copyright &copy; Acko movies (2025)</p>
+    </footer>
+  );
+};
+
+export default Footer;
